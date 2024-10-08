41794e565dfd054b132e66cc1ce432c7e52cedc3
Jorge Machado <jorge.vmachado@hotmail.com>
2024-10-08 12:02:13 -0300

    fix lint

--- apps/nest-api/.eslintrc.js
+++ apps/nest-api/.eslintrc.js
@@ -8,4 +8,8 @@ module.exports = {
     tsconfigRootDir: __dirname,
     sourceType: 'module',
   },
+  rules: {
+    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/no-unused-vars': 'off',
+  }
 };
--- packages/eslint-config/baseRules.js
+++ packages/eslint-config/baseRules.js
@@ -20,4 +20,9 @@ module.exports = {
         'memberSyntaxSortOrder': ['none', 'all', 'multiple', 'single'],
         'allowSeparatedGroups': true,
     }],
+    '@typescript-eslint/interface-name-prefix': 'off',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/no-unused-vars': 'off',
 }

04a70ad4447daa825ff1d75306765ab74ab5a267
Jorge Machado <jorge.vmachado@hotmail.com>
2024-10-16 15:33:28 -0300

    config build

--- packages/ui/rollup.config.mjs
+++ packages/ui/rollup.config.mjs
@@ -6,44 +6,48 @@ import path from 'path';
 import postcss from 'rollup-plugin-postcss';
 import sass from 'rollup-plugin-sass';
 
-export default {
-    input: glob.sync('src/**/index.ts'),
-    output: {
-        dir: path.dirname('dist/index.js'),
-        format: 'esm',
-        sourcemap: true,
-        preserveModules: true,
-        preserveModulesRoot: 'src'
-    },
-    plugins: [
-        resolve(),
-        commonjs(),
-        typescript({ tsconfig: './tsconfig.json' }),
-        postcss({
-            extensions: ['.css', '.scss'],
-            extract: true,
-            minimize: true,
-            use: [
-                ['sass', {
-                    includePaths: [
-                        'node_modules',
-                        'src/styles',
-                        '@geek/tokens/dist/geek/css/_variables.css',
-                        '@geek/tokens/dist/geek/scss/_variables.scss',
-                    ]
-                }]
-            ]
-        }),
-        sass({
-            insert: true,
-            include: ['**/*.scss', '**/*.css'],
-            options: {
+const plugins = [
+    resolve(),
+    commonjs(),
+    typescript({ tsconfig: './tsconfig.json' }),
+    postcss({
+        extensions: ['.css', '.scss'],
+        extract: true,
+        minimize: true,
+        use: [
+            ['sass', {
                 includePaths: [
                     'node_modules',
-                    'src/styles'
+                    'src/styles',
+                    '@geek/tokens/dist/geek/css/_variables.css',
+                    '@geek/tokens/dist/geek/scss/_variables.scss',
                 ]
-            }
-        })
-    ],
-    external: ['react', 'react-dom']
-};
+            }]
+        ]
+    }),
+    sass({
+        insert: true,
+        include: ['**/*.scss', '**/*.css'],
+        options: {
+            includePaths: [
+                'node_modules',
+                'src/styles'
+            ]
+        }
+    })
+];
+
+export default [
+    {
+        input: glob.sync('src/**/index.ts'),
+        output: {
+            dir: path.dirname('dist/index.js'),
+            format: 'esm',
+            sourcemap: true,
+            preserveModules: true,
+            preserveModulesRoot: 'src'
+        },
+        plugins,
+        external: ['react', 'react-dom']
+    }
+];

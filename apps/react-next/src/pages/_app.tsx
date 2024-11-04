d706195635da349d5aeb014898c91b1518aa6b85
Jorge Machado <jorge.vmachado@hotmail.com>
2024-11-04 14:00:52 -0300

    create pages

--- apps/react-next/src/pages/_app.tsx
+++ apps/react-next/src/pages/_app.tsx
@@ -1,6 +1,12 @@
-import '@/styles/globals.css';
+import '@geek/tokens/dist/geek/css/_variables.css';
+import '@/styles/globals.scss';
 import type { AppProps } from 'next/app';
+import { Page } from '@/layout';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+      <Page>
+        <Component {...pageProps} />
+      </Page>
+  );
 }

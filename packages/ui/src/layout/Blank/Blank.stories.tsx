1b0a003628352f27f8387484f6d494be8324f0a9
Jorge Machado <jorge.vmachado@hotmail.com>
2024-11-06 19:13:34 -0300

    blank storybook layout

---
+++ packages/ui/src/layout/Blank/Blank.stories.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+import {Meta, type StoryObj} from '@storybook/react';
+
+import Blank from './';
+
+const meta = {
+    args: {
+        children: <div><h1>CONTENT</h1></div>
+    },
+    title: 'Layout/Blank',
+    component: Blank,
+
+} satisfies Meta<typeof Blank>;
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+    render: (args) => (
+        <Blank {...args}/>
+    )
+};

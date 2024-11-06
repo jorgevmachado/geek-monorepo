6dd0a58ea25a97bb667ad9e3ff4a1afcddd144bf
Jorge Machado <jorge.vmachado@hotmail.com>
2024-11-06 19:51:32 -0300

    fix command run direct

--- packages/ui/src/layout/index.ts
+++ packages/ui/src/layout/index.ts
@@ -1,6 +1,7 @@
+export * from './interface';
+
 export { default as Default } from './Default';
 export { default as Header } from './Header';
 export { default as Sidebar } from './Sidebar';
 export { default as Auth } from './Auth';
 export { default as Blank } from './Blank';
-export * from './interface';
